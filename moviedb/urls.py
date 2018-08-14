728527268664f623b3d946cc2d2c5d3bec9ab1d5
corpuscallosum <corpuscallosum@localhost.localdomain>
2018-08-14 02:43:28 +0200

    cleaning apps

--- moviedb/urls.py
+++ moviedb/urls.py
@@ -14,13 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
 
 from moviesrestapi import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('movies/', views.MoviesViewSet.as_view(({'get':'list', 'post':'create'}))),
     path('comments/', views.CommentsLC.as_view({'get':'list','post':'create'})),
 ]
