a928dc487d29886bc49d86113b1d9f4a7e054239
corpuscallosum <corpuscallosum@localhost.localdomain>
2018-10-06 15:53:32 +0200

    tsv importer changed to class

--- tsvimporter/reader.py
+++ tsvimporter/reader.py
@@ -1,27 +1,43 @@
 import csv
-import contextlib
 import gzip
 
 
-@contextlib.contextmanager
-def tsv_reader(path):
+class TsvReader:
     """
     Context manager which reads data from TSV files (also from packed with gzip ones.
     Returns dict keys[columns, reader] with columns and iterator, next ones are lists of values
 
-    Keyword parameters:
+    Keyword parameters for initialization:
         path -- path of file which is going to be read
+
+    example use, print all data rows:
+    with TsvReader('tsv-file.tsv') as tsv:
+        for row in tsv.readline():
+            print(row)
     """
-    if path[-3:] == '.gz':
-        with gzip.open(path, 'rt') as tsv_file:
-            data = csv.reader(tsv_file, delimiter='\t')
-            columns = next(data)
-            yield {'columns': columns, 'reader': data}
-    else:
-        with open(path) as tsv_file:
-            data = csv.reader(tsv_file, delimiter='\t')
-            columns = next(data)
-            yield {'columns': columns, 'reader': data}
 
+    def __init__(self, path):
+        self.path = path
 
+    def __enter__(self):
+        self.file = self.open(self.path)
+        self.data = csv.reader(self.file, delimiter='\t')
+        self.columns = next(self.data)
+        return self
 
+    def __exit__(self, *args):
+        self.file.__exit__(*args)
+
+    @staticmethod
+    def open(path):
+        """open given file, can be compresed by gzip"""
+        if path.endswith('.gz'):
+            return gzip.open(path, 'rt')
+        elif path.endswith('.tsv'):
+            return open(path)
+        else:
+            raise ValueError('Wrong file extension')
+
+    def readline(self):
+        """returns tsv data iterator"""
+        return self.data
--- tsvimporter/tests.py
+++ tsvimporter/tests.py
@@ -1,41 +1,58 @@
 from django.test import TestCase
-from tsvimporter.reader import tsv_reader
+from tsvimporter.reader import TsvReader
+
 
 class TestTsvImporter(TestCase):
-    def test_read_title_tsv(self):
-        # with TsvReader('/home/corpuscallosum/workspace/moviedb/title.basics.tsv') as tsv:
-        #     print('hello')
-        # tsv_reader('')
-        # for row in tsv_reader('/home/corpuscallosum/workspace/moviedb/title.basics.tsv'):
-        #     print(row)
-        i=0
-        with tsv_reader('/home/corpuscallosum/workspace/moviedb/title.basics.tsv.gz') as tsv:
-            #columns = next(tsv)
-            print(tsv['columns'])
-            for row in tsv['reader']:
-                print(row)
-                i+=1
-                if i==10:
+    def test_read_title_tsv_in_gzip(self):
+        expedted_columns = ['tconst', 'titleType', 'primaryTitle', 'originalTitle', 'isAdult', 'startYear', 'endYear',
+                            'runtimeMinutes', 'genres']
+        expected_rows = [
+            ['tt0000001', 'short', 'Carmencita', 'Carmencita', '0', '1894', '\\N', '1', 'Documentary,Short'],
+            ['tt0000002', 'short', 'Le clown et ses chiens', 'Le clown et ses chiens', '0', '1892', '\\N', '5',
+             'Animation,Short'],
+            ['tt0000003', 'short', 'Pauvre Pierrot', 'Pauvre Pierrot', '0', '1892', '\\N', '4',
+             'Animation,Comedy,Romance']
+        ]
+        i = 0
+        with TsvReader('title.basics.tsv.gz') as tsv:
+            self.assertEqual(tsv.columns, expedted_columns)
+            for row in tsv.readline():
+                self.assertEqual(row, expected_rows[i])
+                i += 1
+                if i == 2:
                     break
-        self.assertFalse()
 
-    def test_read_name_tsv(self):
-        # with TsvReader('/home/corpuscallosum/workspace/moviedb/title.basics.tsv') as tsv:
-        #     print('hello')
-        # tsv_reader('')
-        # for row in tsv_reader('/home/corpuscallosum/workspace/moviedb/title.basics.tsv'):
-        #     print(row)
-        i=0
-        with tsv_reader('/home/corpuscallosum/workspace/moviedb/name.basics.tsv.gz') as tsv:
-            print(tsv['columns'])
-            for row in tsv['reader']:
-                print(row)
-                i+=1
-                if i==10:
+    def test_read_name_tsv_in_gzip(self):
+        expedted_columns = ['nconst', 'primaryName', 'birthYear', 'deathYear', 'primaryProfession', 'knownForTitles']
+
+        expected_rows = [
+            ['nm0000001', 'Fred Astaire', '1899', '1987', 'soundtrack,actor,miscellaneous',
+             'tt0120689,tt0028333,tt0050419,tt0027125'],
+            ['nm0000002', 'Lauren Bacall', '1924', '2014', 'actress,soundtrack',
+             'tt0037382,tt0038355,tt0040506,tt0055688'],
+            ['nm0000003', 'Brigitte Bardot', '1934', '\\N', 'actress,soundtrack,producer',
+             'tt0063715,tt0049189,tt0057345,tt0059956']
+        ]
+        i = 0
+        with TsvReader('name.basics.tsv.gz') as tsv:
+            self.assertEqual(tsv.columns, expedted_columns)
+            for row in tsv.readline():
+                self.assertEqual(row, expected_rows[i])
+                i += 1
+                if i == 2:
                     break
-        self.assertFalse()
 
-    def test_opening_non_existing_file_rise_error(self):
+    def test_opening_non_existing_file_rises_error(self):
         with self.assertRaises(FileNotFoundError):
-            with tsv_reader('non-existing-file') as tsv:
+            with TsvReader('non-existing-file.tsv') as tsv:
+                print(tsv)
+
+    def test_opening_non_existing_file_rises_error(self):
+        with self.assertRaises(FileNotFoundError):
+            with TsvReader('non-existing-file.tsv.gz') as tsv:
+                print(tsv)
+
+    def test_open_wrong_extension(self):
+        with self.assertRaises(ValueError):
+            with TsvReader('s') as tsv:
                 print(tsv)
