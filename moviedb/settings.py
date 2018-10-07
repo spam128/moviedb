09aeeec192ec9488d668ee7fe2aa377077bfec01
corpuscallosum <corpuscallosum@localhost.localdomain>
2018-10-07 16:49:20 +0200

    added global static pagination for drf

--- moviedb/settings.py
+++ moviedb/settings.py
@@ -118,4 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
+}
+
 django_heroku.settings(locals())
