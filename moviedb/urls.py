8ca57fbcca73537004d54626273da546b80d9a4e
corpuscallosum <corpuscallosum@localhost.localdomain>
2018-10-07 16:48:49 +0200

    urls divided for imdb and omdb

--- moviedb/urls.py
+++ moviedb/urls.py
@@ -1,3 +1,9 @@
-from omdbapi.urls import router
+from omdbapi.urls import router as omdb
+from imdbapi.urls import router as imdb
+from django.urls import include, path
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(r'omdb/', include((omdb.urls, 'omdbapi'))),
+    path(r'imdb/', include((imdb.urls, 'imdbapi'))),
+
+]
